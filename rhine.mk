21d379d179a47691ef300087a648c379c66b50a1
Max Weffers <rcstar6696@gmail.com>
2017-08-30 12:56:13 +0200

    rhine: Add USB HIDL package
    Change-Id: I2c78b7d2dabbecf6d424dcb3bbe50bbd61c4da2b

--- rhine.mk
+++ rhine.mk
@@ -143,6 +143,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# USB
+PRODUCT_PACKAGES += \
+	android.hardware.usb@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg \
