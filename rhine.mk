636ac881d40f2dc9dad64e146ab11d353d99381d
Max Weffers <rcstar6696@gmail.com>
2017-08-30 13:53:18 +0200

    rhine: Add HIDL Thermal HAL
    Change-Id: Iddf62239d775a682c5bd1dc3cb561eb83b5f8a4d

--- rhine.mk
+++ rhine.mk
@@ -143,6 +143,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl
+
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service
