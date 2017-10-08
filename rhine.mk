8065a2f1c05e3408b811bca0c2b42485262c9699
nailyk-fr <jenkins@nailyk.fr>
2017-10-08 18:47:22 +0200

    msm8974-common: hidl: Add camera interfaces
    Change-Id: I4379e9f0b9b899f28735828133d3021614b7d12a

--- rhine.mk
+++ rhine.mk
@@ -74,6 +74,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+	android.hardware.camera.provider@2.4-impl \
+	android.hardware.camera.provider@2.4 \
+	android.hardware.camera.device@1.0 \
     libshim_cald \
     libshim_camera \
     libstlport
