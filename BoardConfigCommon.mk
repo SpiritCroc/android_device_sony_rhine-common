5d1edb2761053e66f05ce223f0e7759c9b555c43
SpiritCroc <dev@spiritcroc.de>
2019-09-15 09:20:51 +0200

    .2 [TMP] rhine: Disable sepolicy
    Change-Id: I1e70831e7557abd13792feedcbdfbad87c8d1bbf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,8 +57,8 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 TARGET_INIT_VENDOR_LIB := libinit_rhine
 
 # SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/sony/rhine-common/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    device/sony/rhine-common/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
